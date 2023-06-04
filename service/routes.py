750024e723178a529a9db90a4e62803c61f60b75
NisalaInduwara <e18266@eng.pdn.ac.lk>
2023-06-04 12:56:11 +0530

    readded jobs

--- service/routes.py
+++ service/routes.py
@@ -7,7 +7,7 @@ This microservice handles the lifecycle of Accounts
 from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status
-#from tests.test_routes import BASE_URL  # HTTP Status Codes
+from tests.test_routes import BASE_URL  # HTTP Status Codes
 from . import app  # Import Flask application
 
 
